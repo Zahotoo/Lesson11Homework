d3a0c0e8584fb0e05e7f36de77717046983e0559
Zahotoo <zjh050330@gmail.com>
2023-10-26 01:24:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# python Homework 2
+# Write a program that asks for length and height of a rectangle
+# and prints the area for you.
+length = int(input("Tell me the length of the rectangle: "))
+height = int(input("Tell me the height of the rectangle: "))
+area = length*height
+print("The rectangle's area is: ", area)
